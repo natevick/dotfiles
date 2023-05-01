6d4f28b0d04aa17d7c97c54d780f2d2a94b73152
Nate Vick <nate.vick@hint.io>
2023-05-01 11:33:36 -0700

    Add apk and yum

--- system_install.sh
+++ system_install.sh
@@ -9,27 +9,22 @@ sudo ()
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo "user_install.sh ran at $(date) from $SCRIPT_DIR" >> $SCRIPT_DIR/install.log
 
-# Add source for RCM
-wget https://thoughtbot.com/thoughtbot.asc && \
-  sudo apt-key add - < thoughtbot.asc && \
-  echo "deb https://apt.thoughtbot.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
+if [ `which apt` ]; then
+  
+  # Add source for RCM
+  wget https://thoughtbot.com/thoughtbot.asc && \
+    sudo apt-key add - < thoughtbot.asc && \
+    echo "deb https://apt.thoughtbot.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
 
-echo "Installing apt things" >> ~/install.log
+  # Install RCM
+  sudo apt-get update
+  sudo apt-get install -o Dpkg::Options::="--force-confold" -yq rcm netcat zsh iproute2
 
-# Install RCM
-sudo apt-get update
-sudo apt-get install -o Dpkg::Options::="--force-confold" -yq rcm netcat zsh
-
-echo "Installing tmux from source" >> ~/install.log
-# Build tmux from source
-TMUX_VERSION=3.2a && \
-  wget https://github.com/tmux/tmux/releases/download/$TMUX_VERSION/tmux-$TMUX_VERSION.tar.gz && \
-  tar xf tmux-$TMUX_VERSION.tar.gz && \
-  rm -f tmux-$TMUX_VERSION.tar.gz && \
-  cd tmux-$TMUX_VERSION && \
-  ./configure && \
-  make && \
-  sudo make install && \
-  cd - && \
-  sudo rm -rf /usr/local/src/tmux-\* && \
-  sudo mv tmux-$TMUX_VERSION /usr/local/src
+elif [ `which apk` ]; then
+   apk add rcm zsh iproute2
+elif [ `which yum` ]; then
+  yum install rcm zsh iproute2
+else
+   echo "UNKNOWN LINUX DISTRO"
+   exit 1
+fi
