82e0da9a66a1af528e03be648b8d7ce388a36fe1
Nate Vick <nate.vick@hint.io>
2023-05-01 11:46:47 -0700

    use curl

--- user_install.sh
+++ user_install.sh
@@ -11,8 +11,7 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && \
 echo "Installing oh my zsh if it does not exist" >> ~/install.log
 
 # Install oh-my-zsh
-sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
-
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 echo "Installing dotfiles with rcup" >> ~/install.log
 
 rcup -d $SCRIPT_DIR -f -B docker tmux.conf zshrc gitconfig
